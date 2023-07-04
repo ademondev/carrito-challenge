50dc4189c29f2006ad89a34f9884c118e205fc2b
Raki <killer.vp.mic@gmail.com>
2023-07-04 08:44:18 -0300

    adding context state to header

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,12 +1,22 @@
+import { useContext } from "react";
+import { CartContext } from "./CartContext";
+
 export const HeaderComponent = () => {
+  const { toggleShowCarritoState, cartArray, gems } = useContext(CartContext);
+
   return (
     <div className="bg-stone-700 py-4 px-8 flex justify-between items-center sticky top-0 shadow-md z-10">
       <h1 className="text-white text-2xl font-bold">🧙‍♂️ Potion Shop</h1>
       <div className="flex gap-2 items-center">
-        <img src="./gem.png" />
-        <span>xxx Gemas</span>
+        <img src="./gem.png" alt="gem"/>
+        <span>{gems} Gemas</span>
       </div>
-      <button className="text-white hover:underline">Ver Carrito (xxx)</button>
+      <button 
+        className="text-white hover:underline"
+        onClick={toggleShowCarritoState} // Use the function directly without an arrow function
+      >
+        Ver Carrito ({cartArray.length})
+      </button>
     </div>
   );
 };
