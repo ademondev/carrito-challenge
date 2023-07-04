fa42b313a5b4e8be7f6877d00435aa74cd7c7e33
Raki <killer.vp.mic@gmail.com>
2023-07-04 08:49:46 -0300

    using context state in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { CarritoComponent } from "./components/CarritoComponent";
 import { HeaderComponent } from "./components/HeaderComponent";
 import { ListadoProductosComponent } from "./components/ListadoProductosComponent";
+import { CartContext } from "./components/CartContext";
+
 
 function App() {
-  const [showCarrito, setShowCarrito] = useState(false);
+  const { showCarritoState } = useContext(CartContext);
+  
   return (
-    <div
-      className="min-h-full bg-fixed"
-      style={{ backgroundImage: "url(background.webp)" }}
-    >
-      <HeaderComponent />
-      <div className="flex justify-center min-h-full">
-        <div className="max-w-lg w-full py-16">
-          {showCarrito ? <CarritoComponent /> : <ListadoProductosComponent />}
+      <div
+        className="min-h-full bg-fixed"
+        style={{ backgroundImage: "url(background.webp)" }}
+      >
+        <HeaderComponent />
+        <div className="flex justify-center min-h-full">
+          <div className="max-w-lg w-full py-16">
+            {showCarritoState ? <CarritoComponent /> : <ListadoProductosComponent />}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
